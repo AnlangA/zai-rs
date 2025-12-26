05a25db609d23708429aee911a35cde5f0a449c4
Realer Mason <2682525840@qq.com>
2025-12-26 22:14:37 +0800

    refactor: rename audio_asr_model.rs to model.rs in audio_to_text module

--- src/model/audio_to_text/model.rs
+++ src/model/audio_to_text/model.rs
@@ -2,5 +2,5 @@ use super::super::traits::*;
 use crate::{define_model_type, impl_model_markers};
 
 // GLM ASR model identifier
-define_model_type!(GlmAsr, "glm-asr");
+define_model_type!(GlmAsr, "glm-asr-2512 ");
 impl_model_markers!(GlmAsr: AudioToText);
